7a8d9f30373026d3bd46fff45a2d5da82a1e9bb0
ianjones763 <ianjones@alumni.stanford.edu>
2020-02-04 15:00:42 -0800

    Updates

---
+++ precache-manifest.ddf2abaafa4a63d307611e16f730c438.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5a777760bba24754058d99d013c762e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c590e1868286338dae5",
+    "url": "/static/css/main.bd73f8c3.chunk.css"
+  },
+  {
+    "revision": "7a63866af0e6257bb8d6",
+    "url": "/static/js/2.5cf6c04c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.5cf6c04c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c590e1868286338dae5",
+    "url": "/static/js/main.ccd045bd.chunk.js"
+  },
+  {
+    "revision": "931686523e4ddd616378",
+    "url": "/static/js/runtime-main.92e1f69d.js"
+  },
+  {
+    "revision": "564d96264a270436b5685d3493cc0316",
+    "url": "/static/media/about-headshot-ian.564d9626.jpg"
+  },
+  {
+    "revision": "fd840388eb91061ee02f486909f2bbc9",
+    "url": "/static/media/avalaunch-project-tile.fd840388.png"
+  },
+  {
+    "revision": "5b3342d24504b19d191c2afc1b6d64f5",
+    "url": "/static/media/country-explainer-project-tile.5b3342d2.png"
+  },
+  {
+    "revision": "a12937d6f0e2dbddb9eb1df7a393e6b8",
+    "url": "/static/media/ianjones-resume.a12937d6.pdf"
+  },
+  {
+    "revision": "6fee219556b52f332d04ef0cf5407a08",
+    "url": "/static/media/podmate-project-tile.6fee2195.png"
+  },
+  {
+    "revision": "964a326f412e749b5e21885b4ce25ddb",
+    "url": "/static/media/quest-project-tile.964a326f.png"
+  },
+  {
+    "revision": "3ebdca54968e8ddc474fc55f85ca1453",
+    "url": "/static/media/the-most-country-lyrics-paper.3ebdca54.pdf"
+  },
+  {
+    "revision": "5a795b4e0aa3bfb0b036f6d2827c3d84",
+    "url": "/static/media/toyota-lstm-presentation.5a795b4e.pdf"
+  },
+  {
+    "revision": "0a2b334a44c84e4caadfa7e33848dd73",
+    "url": "/static/media/toyota-project-tile.0a2b334a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "ianhayashijones.com/precache-manifest.43bdf7c366f2463ac10fe9a3216a7aa2.js"
+  "/precache-manifest.ddf2abaafa4a63d307611e16f730c438.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("ianhayashijones.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
