17fc7ec46c99353b74867165413a4b32a5df1215
ianjones763 <ianjones@alumni.stanford.edu>
2020-03-24 15:22:40 -0700

    changed about for Guild

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -16,8 +16,9 @@ const BACKGROUND_INFO =
 
 const WORK_INFO =
     <div>
-        I currently work as a software engineer on the applied machine learning team at 
-        <span className="about-companyname"> Oracle NetSuite</span>
+        I currently work as a software engineer at 
+        <span className="about-companyname">Guild</span>
+        , building a consumer co-branded credit card system from the ground up.
     </div>
 
 const EDUCATION_INFO = 
